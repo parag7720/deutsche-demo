1e040149b860bbcb809cc2bfca399ccc76d23160
parag7720 <7paragmahajan@gmail.com>
2025-08-05 15:27:59 +0530

    FIrst commit

---
+++ src/test/java/com/example/deutsche_demo/DeutscheDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deutsche_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeutscheDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
